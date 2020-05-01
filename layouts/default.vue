a98f0a7420871211725b95393f5c6a7dac54d2ae
Kevin Regenrek <hello@regenrek.at>
2020-05-01 15:22:10 +0200

    step 1 finished

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="bg-mjgray">
     <Nav />
-    <Sidebar />
     <nuxt />
   </div>
 </template>
 <script>
 import Nav from '~/components/nav/Nav'
-import Sidebar from '~/components/nav/Sidebar'
 
 export default {
   components: {
-    Nav,
-    Sidebar
+    Nav
   }
 }
 </script>
